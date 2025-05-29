7543d664a0d57d4eca4814e6a842ac0dac2d9c71
RuhtraOnCode <arthurnowork@gmail.com>
2025-05-29 09:59:05 -0300

    Implementação de recompensa + mensagem personalizada no evento PlayerJoinEvent

--- src/main/java/org/arthurhtech/paperJoinPlugin/PaperJoinPlugin.java
+++ src/main/java/org/arthurhtech/paperJoinPlugin/PaperJoinPlugin.java
@@ -1,13 +1,13 @@
 package org.arthurhtech.paperJoinPlugin;
 
+import org.arthurhtech.paperJoinPlugin.listeners.JoinListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PaperJoinPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(new JoinListener(), this);
     }
 
     @Override
---
+++ src/main/java/org/arthurhtech/paperJoinPlugin/listeners/JoinListener.java
@@ -0,0 +1,20 @@
+package org.arthurhtech.paperJoinPlugin.listeners;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class JoinListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        player.sendMessage("Bem vindo ao servidor! " + player.getName());
+        player.sendMessage("Aqui, pegue algo para começar.");
+        player.getInventory().addItem(new ItemStack(Material.BREAD, 10));
+        player.getInventory().addItem(new ItemStack(Material.WOODEN_SWORD));
+    }
+}
